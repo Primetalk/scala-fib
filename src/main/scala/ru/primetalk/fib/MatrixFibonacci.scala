3b3067620c9a045ef344da126ea61d0002f13135
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2017-09-12 23:14:52 +0300

    Move matrix to a separate file

--- src/main/scala/ru/primetalk/fib/MatrixFibonacci.scala
+++ src/main/scala/ru/primetalk/fib/MatrixFibonacci.scala
@@ -1,6 +1,7 @@
 package ru.primetalk.fib
 
 import algebra.ring.MultiplicativeSemigroup
+import spire.algebra.{AdditiveAbGroup, MultiplicativeMonoid}
 
 /**
   * An implementation of fibonacci numbers algorithm with performance
@@ -8,51 +9,34 @@ import algebra.ring.MultiplicativeSemigroup
   *
   */
 object MatrixFibonacci {
-  /** Condensed representation of a symmetric matrix 2x2 (a,b;b,c).
-    * We just need to implement multiplication.
-   */
-  case class SymmetricMat22[Number](a: Number, b: Number, c: Number)
 
-  implicit def symmetricMat22IsMultiplicativeSemigroup[Number: Numeric]: MultiplicativeSemigroup[SymmetricMat22[Number]] = new MultiplicativeSemigroup[SymmetricMat22[Number]] {
-    private val num = implicitly[Numeric[Number]]
-    /** Product of symmetric matrices is again symmetric. So we can use the same
-      * condensed matrix representation.
-      * */
-    override def times(x: SymmetricMat22[Number], y: SymmetricMat22[Number]): SymmetricMat22[Number] = {
-      val be = num.times(x.b, y.b)
-      SymmetricMat22[Number](
-        num.plus(num.times(x.a, y.a), be),
-        num.plus(num.times(x.a, y.b), num.times(x.b, y.c)),
-        num.plus(be, num.times(x.c, y.c))
-      )
-    }
-  }
   /**
     * @tparam Number sufficient number representation
     * @return n-th Fibonacci number
     */
-  final def apply[Number: Numeric](n: Int)(f0: Number, f1: Number): Number = {
-    val num = implicitly[Numeric[Number]]
+  final def apply[Number: MultiplicativeMonoid: AdditiveAbGroup](n: Int)(f0: Number, f1: Number): Number = {
+    val m = implicitly[MultiplicativeMonoid[Number]]
+    val a = implicitly[AdditiveAbGroup[Number]]
 
     val mat = implicitly[MultiplicativeSemigroup[SymmetricMat22[Number]]]
 
     type PosInt = Int
 
-    val producerMatrix = SymmetricMat22[Number](num.one, num.one, num.zero) // we'll power this matrix
+    val producerMatrix = SymmetricMat22[Number](m.one, m.one, a.zero) // we'll power this matrix
 
-    def apply0(n: PosInt)(f0: Number, f1: Number): Number = {
-      mat.pow(producerMatrix, n).a
-    }
+    def powA(n: PosInt): Number = mat.pow(producerMatrix, n).a
 
     n match {
       case _ if n == 0 =>
         f0
+      case _ if n == 1 || n == -1 =>
+        f1
       case _ if n > 0 =>
-        apply0(n - 1)(f0, f1)
+        powA(n - 1)
       case _ if (-n + 1) % 2 == 0 =>
-        apply0(-n + 1)(f0, f1)
+        powA(-n + 1)
       case _ =>
-        num.negate(apply0(-n + 1)(f0, f1))
+        a.negate(powA(-n + 1))
     }
   }
 
---
+++ src/main/scala/ru/primetalk/fib/SymmetricMat22.scala
@@ -0,0 +1,27 @@
+package ru.primetalk.fib
+
+import algebra.ring.{AdditiveSemigroup, MultiplicativeSemigroup}
+
+/** Condensed representation of a symmetric matrix 2x2 (a,b;b,c).
+  * We just need to implement multiplication.
+  */
+case class SymmetricMat22[Number](a: Number, b: Number, c: Number)
+
+object SymmetricMat22 {
+  implicit def symmetricMat22IsMultiplicativeSemigroup[Number: MultiplicativeSemigroup: AdditiveSemigroup]: MultiplicativeSemigroup[SymmetricMat22[Number]] = new MultiplicativeSemigroup[SymmetricMat22[Number]] {
+    import spire.syntax.numeric.{additiveSemigroupOps, multiplicativeSemigroupOps}
+
+    /** Product of symmetric matrices is again symmetric. So we can use the same
+      * condensed matrix representation.
+      */
+    override def times(x: SymmetricMat22[Number], y: SymmetricMat22[Number]): SymmetricMat22[Number] = {
+      val be = x.b * y.b
+      SymmetricMat22[Number](
+        x.a * y.a + be,
+        x.a * y.b + x.b * y.c,
+        be + x.c * y.c
+      )
+    }
+  }
+
+}

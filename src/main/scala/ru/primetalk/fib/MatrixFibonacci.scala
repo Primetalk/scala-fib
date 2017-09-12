c042ad8dd33de9a4a30c468f47232ade89779638
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2017-09-12 22:50:57 +0300

    Implement matrix matrix operations as multiplicative semigroup

--- src/main/scala/ru/primetalk/fib/MatrixFibonacci.scala
+++ src/main/scala/ru/primetalk/fib/MatrixFibonacci.scala
@@ -1,6 +1,6 @@
 package ru.primetalk.fib
 
-import scala.annotation.tailrec
+import algebra.ring.MultiplicativeSemigroup
 
 /**
   * An implementation of fibonacci numbers algorithm with performance
@@ -9,59 +9,52 @@ import scala.annotation.tailrec
   */
 object MatrixFibonacci {
   /** Condensed representation of a symmetric matrix 2x2 (a,b;b,c).
+    * We just need to implement multiplication.
    */
   case class SymmetricMat22[Number](a: Number, b: Number, c: Number)
 
+  implicit def symmetricMat22IsMultiplicativeSemigroup[Number: Numeric]: MultiplicativeSemigroup[SymmetricMat22[Number]] = new MultiplicativeSemigroup[SymmetricMat22[Number]] {
+    private val num = implicitly[Numeric[Number]]
+    /** Product of symmetric matrices is again symmetric. So we can use the same
+      * condensed matrix representation.
+      * */
+    override def times(x: SymmetricMat22[Number], y: SymmetricMat22[Number]): SymmetricMat22[Number] = {
+      val be = num.times(x.b, y.b)
+      SymmetricMat22[Number](
+        num.plus(num.times(x.a, y.a), be),
+        num.plus(num.times(x.a, y.b), num.times(x.b, y.c)),
+        num.plus(be, num.times(x.c, y.c))
+      )
+    }
+  }
   /**
     * @tparam Number sufficient number representation
-    * @return
+    * @return n-th Fibonacci number
     */
   final def apply[Number: Numeric](n: Int)(f0: Number, f1: Number): Number = {
     val num = implicitly[Numeric[Number]]
 
+    val mat = implicitly[MultiplicativeSemigroup[SymmetricMat22[Number]]]
+
     type PosInt = Int
-    type Mat = SymmetricMat22[Number]
-    def Mat(a: Number, b: Number, c: Number) = SymmetricMat22[Number](a, b, c)
 
-    val one = num.one
-    val zero = num.zero
-    val producerMatrix = SymmetricMat22[Number](one, one, zero) // we'll power this matrix
-    val identityMatrix = SymmetricMat22[Number](one, zero, one)
-
-    def mul(m1: Mat, m2: Mat): Mat = {
-      val be = num.times(m1.b, m2.b)
-      Mat(
-        num.plus(num.times(m1.a, m2.a), be),
-        num.plus(num.times(m1.a, m2.b), num.times(m1.b, m2.c)),
-        num.plus(be, num.times(m1.c, m2.c))
-      )
-    }
-    @tailrec
-    def powAndMultLog(n: PosInt)(mat: Mat, multiplyAtTheEnd: Mat): Mat = n match {
-      case 0 => multiplyAtTheEnd
-      case 1 => mul(mat, multiplyAtTheEnd)
-      case _ if n % 2 == 0 => powAndMultLog(n / 2)(mul(mat, mat), multiplyAtTheEnd)
-      case _ if n % 2 == 1 => powAndMultLog(n / 2)(mul(mat, mat), mul(mat, multiplyAtTheEnd))
-    }
-    @tailrec
-    def powAndMultLinear(n: PosInt)(mat: Mat, multiplyAtTheEnd: Mat): Mat = n match {
-      case 0 => multiplyAtTheEnd
-      case _ => powAndMultLinear(n-1)(mat, mul(mat, multiplyAtTheEnd))
-    }
+    val producerMatrix = SymmetricMat22[Number](num.one, num.one, num.zero) // we'll power this matrix
 
     def apply0(n: PosInt)(f0: Number, f1: Number): Number = {
-      powAndMultLog(n)(producerMatrix, identityMatrix).a
+      mat.pow(producerMatrix, n).a
     }
+
     n match {
       case _ if n == 0 =>
         f0
       case _ if n > 0 =>
-        apply0(n-1)(f0, f1)
-      case _ if (-n+1) % 2 == 0 =>
-        apply0(-n+1)(f0, f1)
+        apply0(n - 1)(f0, f1)
+      case _ if (-n + 1) % 2 == 0 =>
+        apply0(-n + 1)(f0, f1)
       case _ =>
-        num.negate(apply0(-n+1)(f0, f1))
+        num.negate(apply0(-n + 1)(f0, f1))
     }
   }
 
 }
+
